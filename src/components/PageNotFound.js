141cd21b506db2f8f7c98f9bc8df410c4ddd12bd
Adithya Saish <adiwithmj@gmail.com>
2022-05-09 17:07:14 +0530

    Initital commit

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,3 @@
+import React from "react";
+const PageNotFound = () => <h1> Oops! Page Not Found </h1>;
+export default PageNotFound;
---
+++ webpack.config.dev.js
@@ -0,0 +1,45 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPLugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  devtool: "cheap-module-source-map", // source maps lets you see the original code when debugging in the browser
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    stats: "minimal",
+    overlay: true, // overlay all errors
+    historyApiFallback: true, // route all pi calls through index.html so that react router can be used to route between pages
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.ico",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"],
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
