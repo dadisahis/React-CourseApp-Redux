b38a090214c76b0262d7deb57e19a734aae8c52b
Adithya Saish <adiwithmj@gmail.com>
2022-05-30 18:59:43 +0530

    Added Course page,courseForm and associated actions

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -8,12 +8,36 @@ export function createCourse(course) {
 export function loadCourseSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
+
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export function updateCourseSuccess(courses) {
+  return { type: types.UPDATE_COURSE_SUCCESS, courses };
+}
+
 export function loadCourses() {
   return function (dispatch) {
     return courseApi
       .getCourses()
       .then((courses) => {
         dispatch(loadCourseSuccess(courses));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export function saveCourse(course) {
+  return function (dispatch, getState) {
+    return courseApi
+      .saveCourse(course)
+      .then((savedCourse) => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
       })
       .catch((error) => {
         throw error;
